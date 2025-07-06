0bb2cf6386cfa36936c8a60f07ec0e82f6478fde
Kevin-Tan2 <kevintan314159@gmail.com>
2025-07-06 23:05:57 +1000

    Removed Telescope dependency in the lspconfig

--- init.lua
+++ init.lua
@@ -1,8 +1,9 @@
-local vim_config_path = vim.fn.stdpath("config")
-
 require("config.basic-settings")
 require("config.ftdetect")
 require("plugins")
+
+-- Must be called after plugins
+require("config.lsp")
 
 vim.cmd.colorscheme("dracula")
 
@@ -15,15 +16,3 @@ highlight Normal guibg=none
 "highlight NonText ctermbg=none
 
 ]]
-
-
--- Load all LSP's in "lua/config/lsp"
----local lsp_path = vim_config_path .. "/lua/config/lsp"
----
------ Then load all other LSP configs
----for _, file in ipairs(vim.fn.readdir(lsp_path)) do
----    if file:match("%.lua$") and file ~= "global.lua" then
----        local module_name = "config.lsp." .. file:gsub("%.lua$", "")
----        require(module_name)
----    end
----end
--- lua/config/lsp/init.lua
+++ lua/config/lsp/init.lua
@@ -18,7 +18,7 @@ local lsp_path = vim_config_path .. "/lua/config/lsp"
 
 -- Then load all other LSP configs
 for _, file in ipairs(vim.fn.readdir(lsp_path)) do
-    if file:match("%.lua$") and file ~= "global.lua" then
+    if file:match("%.lua$") and file ~= "global.lua" and file ~= "init.lua" then
         local module_name = "config.lsp." .. file:gsub("%.lua$", "")
         require(module_name)
     end
